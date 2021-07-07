227a01b42c24630ec0dcc7cc40ca8c64e7dfd649
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-07 16:42:24 +0200

    Allow the Header component to have a title.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <Header />
+    <Header title="Task Tracker" />
   </div>
 </template>
 
@@ -10,7 +10,7 @@ import Header from './components/Header'
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
   <header>
-    <h1>Task Tracker</h1>
+    <h1>{{ title }}</h1>
   </header>
 </template>
 
 <script>
   export default {
     name: 'Header',
+    props: {
+      title: {
+        type: String,
+        default: 'Hello World',
+      },
+    }
   }
 </script>
 
