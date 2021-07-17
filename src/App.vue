8b77a7ef82ee3377ec0cdfbd82da486b037ed5ed
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-17 17:09:25 +0200

    Add tasks through API.

--- src/App.vue
+++ src/App.vue
@@ -32,8 +32,17 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
     },
-    addTask(task) {
-      this.tasks = [ ...this.tasks, task]
+    async addTask(task) {
+      const response = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      })
+
+      const data = await response.json()
+      this.tasks = [ ...this.tasks, data]
     },
     deleteTask(id) {
       if(confirm("Are you sure?")) {
--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -36,7 +36,7 @@
         }
 
         const newTask = {
-          id: Math.floor(Math.random() * 100000),
+          //id: Math.floor(Math.random() * 100000),
           text: this.text,
           day: this.day,
           reminder: this.reminder
