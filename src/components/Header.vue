44afd5de63b2ef51889850ce5020babea66ec617
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-17 15:55:31 +0200

    Refactor button click.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'black' : 'green'" @toggle-add-task="$emit('toggle-add-task')" />
+    <Button :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'black' : 'green'" @button-click="$emit('toggle-add-task')" />
   </header>
 </template>
 
