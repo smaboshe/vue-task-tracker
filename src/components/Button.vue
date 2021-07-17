2b7e26e044b2b32e562630532309fc088737259b
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-17 08:19:34 +0200

    Console log button clicks.

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :style="{background: color}" class="btn">{{ text }}</button>
+  <button class="btn" :style="{ background: color }" @click="onClick()">{{ text }}</button>
 </template>
 
 <script>
@@ -8,6 +8,11 @@ export default {
   props: {
     text:  String,
     color: String
+  },
+  methods: {
+    onClick() {
+      console.log('Click')
+    }
   }
 }
 </script>
