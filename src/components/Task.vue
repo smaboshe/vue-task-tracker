aff3cdb7c0d4f15006fe0ed4a089a68c393ce13d
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-17 12:22:13 +0200

    Console log the ID of the task that we clicked and want to delete.

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -2,7 +2,7 @@
   <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>
       {{ task.text }}
-      <i class="fas fa-times"></i>
+      <i @click="onDelete(task.id)" class="fas fa-times"></i>
     </h3>  
     <p>{{ task.day }}</p>
   </div>
@@ -13,6 +13,11 @@
     name: 'Tassk',
     props: {
       task: Object
+    },
+    methods: {
+      onDelete(id) {
+        console.log(id)
+      }
     }
   }
 </script>
