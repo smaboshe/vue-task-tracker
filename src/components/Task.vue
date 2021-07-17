034a2e3eef33db7d31ce277b55bd5118c038de2e
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-17 14:30:47 +0200

    Refactor on-delete.

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -2,7 +2,7 @@
   <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>
       {{ task.text }}
-      <i @click="onDelete(task.id)" class="fas fa-times"></i>
+      <i class="fas fa-times" @click="$emit('delete-task', task.id)"></i>
     </h3>  
     <p>{{ task.day }}</p>
   </div>
@@ -14,11 +14,6 @@
     props: {
       task: Object
     },
-    methods: {
-      onDelete(id) {
-        this.$emit('delete-task', id)
-      }
-    }
   }
 </script>
 
