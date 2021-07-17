9262f2e8e252fdcd58d2f2493740333d8521a68a
Silumesii Maboshe <silumesii@bongohive.co.zm>
2021-07-17 15:08:35 +0200

    Add Add Task form.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <AddTask />
     <Tasks :tasks="tasks" @delete-task="deleteTask" @toggle-reminder="toggleReminder"  />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
     Header,
     Tasks,
+    AddTask,
   },
   data() {
     return {
---
+++ src/components/AddTask.vue
@@ -0,0 +1,61 @@
+<template>
+  <form class="add-form">
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>Day and Time</label>
+      <input type="text" name="day" placeholder="Add Day and Time" />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" name="reminder" />
+    </div>
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'AddTask',
+  }
+</script>
+
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+
+.form-control {
+  margin: 20px 0;
+}
+
+.form-control label {
+  display: block;
+}
+
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.form-control-check label {
+  flex: 1;
+}
+
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
